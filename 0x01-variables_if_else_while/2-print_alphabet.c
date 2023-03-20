dd863c0e04e8b769b098458d4001d09d2d253f8f
daniel-wesh <daniel.waweru@outlook.com>
2023-03-19 18:23:34 -0700

    Print letters of the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+
+	putchar('\n');
+	return (0);
+}
