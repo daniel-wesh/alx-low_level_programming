877434eb0d8a8bd054285060b2a2c34af5e9dec7
daniel-wesh <daniel.waweru@outlook.com>
2023-03-21 05:21:16 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
+ *print_alphabet - function that prints alphabet letters in lowercase
  *
- * Return: Always 0.
+ * Return: void
  */
 void print_alphabet(void)
 {
