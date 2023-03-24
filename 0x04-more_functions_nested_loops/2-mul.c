0e4ca4d933813d5992f4b4e78372bd29a156b4e7
daniel-wesh <daniel.waweru@outlook.com>
2023-03-24 10:20:37 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - function that multiplies two numbers
+ *@a: first param
+ *@b: second param
+ * Return: product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
