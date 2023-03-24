2dfe1c2c12b5a3fae4643bd3a2a871de6c49940b
daniel-wesh <daniel.waweru@outlook.com>
2023-03-24 10:00:47 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper - function that checks if a character is uppercase
+ *@c: parameter
+ *
+ * Return: 1 if c is uppercase, 0 if lowercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
