78aff7b2f042fdb1c1f05e3a09dbb991b3314a88
daniel-wesh <daniel.waweru@outlook.com>
2023-04-04 11:53:14 -0700

    function that prints the first occurence of a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Returns a pointer to the first occurrence of the character c in the string s
+ * @s: parameter 1
+ * @c: parameter 2
+ *
+ * Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	for (n = 0; s[n] >= '\0'; n++)
+	{
+		if (s[n] == c)
+			return (s + n);
+	}
+
+	return (NULL);
+}
